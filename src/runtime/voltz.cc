b38efae9239dd404221e41b2c16135c14ddd3801
Jack Maloney <jmmaloney4@gmail.com>
2015-09-19 21:49:19 -0500

    Add vz_array unbox

--- src/runtime/voltz.cc
+++ src/runtime/voltz.cc
@@ -5,6 +5,7 @@
 // obtain one at http://mozilla.org/MPL/2.0/.
 
 #include "voltz-internal.h"
+#include <stdlib.h>
 
 id vz_bool_boxI(bool value) {
     id boolcls = vz_class_get("std::Bool");
@@ -23,3 +24,16 @@ bool vz_bool_unboxI(id obj) {
 }
 
 bool (*vz_bool_unbox)(id) = vz_bool_unboxI;
+
+id* vz_array_unboxI(id obj) {
+    id count = vz_msg_send(obj, "Count", 0);
+    NUM c = vz_num_unbox(count);
+    id* rv = (id*) malloc(sizeof(id) * c);
+    for (NUM k = 0; k < c; k++) {
+        rv[(int64_t)k] = vz_msg_send(obj->ivars[0].obj, "Retain", 0);
+    }
+    vz_msg_send(count, "Release", 0);
+    return rv;
+}
+
+id*(*vz_array_unbox)(id) = vz_array_unboxI;
