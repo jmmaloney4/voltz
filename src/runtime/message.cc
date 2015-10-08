fe0d7a22e7742026a6a6cef5a3ba47c97bc0328f
Jack Maloney <jmmaloney4@gmail.com>
2015-10-08 15:19:53 -0500

    update formatting

--- src/runtime/message.cc
+++ src/runtime/message.cc
@@ -8,16 +8,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-extern "C" id (*vz_msg_send_super)(id target, const char *sel, NUM argc, ...);
-extern "C" id (*vz_msg_send_super_v)(id target, const char *sel, NUM argc,
+extern "C" id (*vz_msg_send_super)(id target, const char* sel, NUM argc, ...);
+extern "C" id (*vz_msg_send_super_v)(id target,
+                                     const char* sel,
+                                     NUM argc,
                                      va_list ap);
-extern "C" id (*vz_msg_send_super_a)(id target, const char *sel, NUM argc,
-                                     id *args);
+extern "C" id (*vz_msg_send_super_a)(id target,
+                                     const char* sel,
+                                     NUM argc,
+                                     id* args);
 extern "C" id (*vz_msg_send_super_s)(id target, SEL sel, NUM argc, ...);
 extern "C" id (*vz_msg_send_super_sv)(id target, SEL sel, NUM argc, va_list ap);
-extern "C" id (*vz_msg_send_super_sa)(id target, SEL sel, NUM argc, id *args);
+extern "C" id (*vz_msg_send_super_sa)(id target, SEL sel, NUM argc, id* args);
 
-id vz_msg_sendI(id target, const char *sel, NUM argc, ...) {
+id vz_msg_sendI(id target, const char* sel, NUM argc, ...) {
     va_list ap;
     va_start(ap, argc);
     id rv = vz_msg_send_sv(target, vz_sel_get(sel), argc, ap);
@@ -25,11 +29,11 @@ id vz_msg_sendI(id target, const char *sel, NUM argc, ...) {
     return rv;
 }
 
-id vz_msg_send_vI(id target, const char *sel, NUM argc, va_list ap) {
+id vz_msg_send_vI(id target, const char* sel, NUM argc, va_list ap) {
     return vz_msg_send_sv(target, vz_sel_get(sel), argc, ap);
 }
 
-id vz_msg_send_aI(id target, const char *sel, NUM argc, id *args) {
+id vz_msg_send_aI(id target, const char* sel, NUM argc, id* args) {
     return vz_msg_send_sa(target, vz_sel_get(sel), argc, args);
 }
 
@@ -42,23 +46,23 @@ id vz_msg_send_sI(id target, SEL sel, NUM argc, ...) {
 }
 
 id vz_msg_send_svI(id target, SEL sel, NUM argc, va_list ap) {
-    id *args = (id *)alloca(sizeof(id) * argc);
+    id* args = (id*) alloca(sizeof(id) * argc);
     for (NUM k = 0; k < argc; k++) {
-        args[(int64_t)k] = va_arg(ap, id);
+        args[(int64_t) k] = va_arg(ap, id);
     }
 
     return vz_msg_send_sa(target, sel, argc, args);
 }
 
-id vz_msg_send_saI(id target, SEL sel, NUM argc, id *args) {
+id vz_msg_send_saI(id target, SEL sel, NUM argc, id* args) {
     if (target == nil) {
         return nil;
     }
 
     for (id c = target->isa; c != nil; c = vz_class_super(c)) {
         for (NUM k = 0; k < vz_class_mthdc(c); k++) {
-            if (c->ivars[7].arr[(int64_t)k]->ivars[0].sel == sel) {
-                return c->ivars[7].arr[(int64_t)k]->ivars[1].imp->operator()(
+            if (c->ivars[7].arr[(int64_t) k]->ivars[0].sel == sel) {
+                return c->ivars[7].arr[(int64_t) k]->ivars[1].imp->operator()(
                     target, sel, argc, args);
             }
         }
@@ -69,19 +73,25 @@ id vz_msg_send_saI(id target, SEL sel, NUM argc, id *args) {
     return nil;
 }
 
-id (*vz_msg_send)(id target, const char *sel, NUM argc, ...) = vz_msg_sendI;
-id (*vz_msg_send_v)(id target, const char *sel, NUM argc,
+id (*vz_msg_send)(id target, const char* sel, NUM argc, ...) = vz_msg_sendI;
+id (*vz_msg_send_v)(id target,
+                    const char* sel,
+                    NUM argc,
                     va_list ap) = vz_msg_send_vI;
-id (*vz_msg_send_a)(id target, const char *sel, NUM argc,
-                    id *args) = vz_msg_send_aI;
+id (*vz_msg_send_a)(id target,
+                    const char* sel,
+                    NUM argc,
+                    id* args) = vz_msg_send_aI;
 id (*vz_msg_send_s)(id target, SEL sel, NUM argc, ...) = vz_msg_send_sI;
-id (*vz_msg_send_sv)(id target, SEL sel, NUM argc,
+id (*vz_msg_send_sv)(id target,
+                     SEL sel,
+                     NUM argc,
                      va_list ap) = vz_msg_send_svI;
-id (*vz_msg_send_sa)(id target, SEL sel, NUM argc, id *args) = vz_msg_send_saI;
+id (*vz_msg_send_sa)(id target, SEL sel, NUM argc, id* args) = vz_msg_send_saI;
 
 // vz_msg_super
 
-id vz_msg_send_superI(id target, const char *sel, NUM argc, ...) {
+id vz_msg_send_superI(id target, const char* sel, NUM argc, ...) {
     va_list ap;
     va_start(ap, argc);
     id rv = vz_msg_send_super_sv(target, vz_sel_get(sel), argc, ap);
@@ -89,11 +99,11 @@ id vz_msg_send_superI(id target, const char *sel, NUM argc, ...) {
     return rv;
 }
 
-id vz_msg_send_super_vI(id target, const char *sel, NUM argc, va_list ap) {
+id vz_msg_send_super_vI(id target, const char* sel, NUM argc, va_list ap) {
     return vz_msg_send_super_sv(target, vz_sel_get(sel), argc, ap);
 }
 
-id vz_msg_send_super_aI(id target, const char *sel, NUM argc, id *args) {
+id vz_msg_send_super_aI(id target, const char* sel, NUM argc, id* args) {
     return vz_msg_send_super_sa(target, vz_sel_get(sel), argc, args);
 }
 
@@ -106,42 +116,57 @@ id vz_msg_send_super_sI(id target, SEL sel, NUM argc, ...) {
 }
 
 id vz_msg_send_super_svI(id target, SEL sel, NUM argc, va_list ap) {
-    id *args = (id *)alloca(sizeof(id) * argc);
+    id* args = (id*) alloca(sizeof(id) * argc);
     for (NUM k = 0; k < argc; k++) {
-        args[(int64_t)k] = va_arg(ap, id);
+        args[(int64_t) k] = va_arg(ap, id);
     }
 
     return vz_msg_send_super_sa(target, sel, argc, args);
 }
 
-id vz_msg_send_super_saI(id target, SEL sel, NUM argc, id *args) {
+id vz_msg_send_super_saI(id target, SEL sel, NUM argc, id* args) {
     if (target == nil) {
         return nil;
     }
 
     for (id c = target->isa->ivars[0].obj; c != nil; c = vz_class_super(c)) {
         for (NUM k = 0; k < vz_class_mthdc(c); k++) {
-            if (c->ivars[7].arr[(int64_t)k]->ivars[0].sel == sel) {
-                return c->ivars[7].arr[(int64_t)k]->ivars[1].imp->operator()(
+            if (c->ivars[7].arr[(int64_t) k]->ivars[0].sel == sel) {
+                return c->ivars[7].arr[(int64_t) k]->ivars[1].imp->operator()(
                     target, sel, argc, args);
             }
         }
     }
 
-    fprintf(stderr, "%s (%p) does not implement the method %s\n",
-            vz_class_name(target->isa), target->isa, sel->value);
+    fprintf(stderr,
+            "%s (%p) does not implement the method %s\n",
+            vz_class_name(target->isa),
+            target->isa,
+            sel->value);
     abort();
 }
 
-id (*vz_msg_send_super)(id target, const char *sel, NUM argc,
+id (*vz_msg_send_super)(id target,
+                        const char* sel,
+                        NUM argc,
                         ...) = vz_msg_send_superI;
-id (*vz_msg_send_super_v)(id target, const char *sel, NUM argc,
+id (*vz_msg_send_super_v)(id target,
+                          const char* sel,
+                          NUM argc,
                           va_list ap) = vz_msg_send_super_vI;
-id (*vz_msg_send_super_a)(id target, const char *sel, NUM argc,
-                          id *args) = vz_msg_send_super_aI;
-id (*vz_msg_send_super_s)(id target, SEL sel, NUM argc,
+id (*vz_msg_send_super_a)(id target,
+                          const char* sel,
+                          NUM argc,
+                          id* args) = vz_msg_send_super_aI;
+id (*vz_msg_send_super_s)(id target,
+                          SEL sel,
+                          NUM argc,
                           ...) = vz_msg_send_super_sI;
-id (*vz_msg_send_super_sv)(id target, SEL sel, NUM argc,
+id (*vz_msg_send_super_sv)(id target,
+                           SEL sel,
+                           NUM argc,
                            va_list ap) = vz_msg_send_super_svI;
-id (*vz_msg_send_super_sa)(id target, SEL sel, NUM argc,
-                           id *args) = vz_msg_send_super_saI;
+id (*vz_msg_send_super_sa)(id target,
+                           SEL sel,
+                           NUM argc,
+                           id* args) = vz_msg_send_super_saI;
